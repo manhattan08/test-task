7fc174863cdaf8c70d216a50803773139de7affc
manhattan <andrey2004uor@gmail.com>
2023-05-29 10:47:04 +0300

    config react

--- client/src/utils/consts.js
+++ client/src/utils/consts.js
@@ -1,3 +1,3 @@
 export const POKE_API_URL = "https://pokeapi.co/api/v2/"
 export const PER_PAGE = 20
-export const SERVER_API = "http://localhost:7000/"
+export const SERVER_API = "https://test-task-wheat.vercel.app/"
